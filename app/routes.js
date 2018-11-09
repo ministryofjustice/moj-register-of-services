6c5f5736c70eb3168ef2d7a7b13095e88190b5a8
Simon Whatley <simonwhatley@gmail.com>
2018-11-09 14:42:14 +0000

    Added routes and basic layouts

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,39 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.get('/', (req, res) => {
+	res.render('index',
+		{
+			data: {
+				organisations: ''
+			}
+		});
+});
+
+router.get('/:organisation/', (req, res) => {
+	res.render('organisation',
+		{
+			links: {
+				'back': `${req.baseUrl}/`
+			},
+			data: {
+				organisation: '',
+				services: ''
+			}
+		});
+});
+
+router.get('/:organisation/:service/', (req, res) => {
+	res.render('service',
+		{
+			links: {
+				'back': `${req.baseUrl}/${req.params.organisation}/`
+			},
+			data: {
+				organisation: '',
+				service: ''
+			}
+		});
+});
 
 module.exports = router
